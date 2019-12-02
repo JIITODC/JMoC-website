25782c14c5fbe2b7edf4a4ce1693d7f189e281e1
Divyansh Kamboj <kambojdivyansh2000@gmail.com>
2019-12-02 19:34:14 +0530

    Create from scratch
    Signed-off-by: Divyansh Kamboj <kambojdivyansh2000@gmail.com>

---
+++ controllers/projects.js
@@ -0,0 +1,30 @@
+const Project = require('../models/Project');
+
+exports.getAllProjects = (req, res) => {
+  Project.find({}, (err, projects) => {
+    res.render('projects', {
+      title: 'Projects',
+      projects
+    });
+  });
+  // res.render('home', { title: 'Hello' });
+};
+
+exports.getNewProject = (req, res) => {
+  res.render('project/new_project', { title: 'New Project' });
+};
+
+exports.postNewProject = (req, res, next) => {
+  const project = new Project({
+    title: req.body.title,
+    description: req.body.description,
+    url: req.body.url,
+    mentor: req.body.url
+  });
+  project.save((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/project');
+  });
+};
