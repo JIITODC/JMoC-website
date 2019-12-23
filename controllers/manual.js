dc5ff8566ffc606974b7269fa9379e6c29dc7eef
Divyansh Kamboj <kambojdivyansh2000@gmail.com>
2019-12-24 00:37:44 +0530

    Manuals
    Signed-off-by: Divyansh Kamboj <kambojdivyansh2000@gmail.com>

--- controllers/manual.js
+++ controllers/manual.js
@@ -1,8 +1,12 @@
 
 exports.getStudentManual = (req, res) => {
-  res.render('man_student');
+  res.render('man_student', {
+    title: 'Student Manual'
+  });
 };
 
 exports.getMentorManual = (req, res) => {
-  res.render('man_mentor');
+  res.render('man_mentor', {
+    title: 'Mentor Manual'
+  });
 };
