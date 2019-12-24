bd4b57e8f17f38a0fcd60f7a3b60fa855fb16fd4
karandevtyagi <vatstyagi999@gmail.com>
2019-12-24 21:06:23 +0530

    redirect error update

--- controllers/home.js
+++ controllers/home.js
@@ -3,9 +3,9 @@
  * Home page.
  */
 exports.index = (req, res) => {
-  if (req.user) {
-    return res.redirect('/account');
-  }
+  // if (req.user) {
+  //   return res.redirect('/account');
+  // }
   res.render('home', {
     title: 'Home'
   });
