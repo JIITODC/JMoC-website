941eb69905d48e1ede32c3095f25d7f66f03f180
terabo <yashsrivastava85p@gmail.com>
2020-01-10 19:11:49 +0530

    Few more changes

---
+++ JMoC-website-master/models/Project.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  url: String,
+  mentor: String,
+  email: String
+});
+
+const Project = mongoose.model('Project', projectSchema);
+
+module.exports = Project;
